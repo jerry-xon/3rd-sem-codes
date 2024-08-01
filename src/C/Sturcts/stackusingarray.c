f0e89777c9520798c573c40be6a3c19b375d35e8
Jayesh Purswani <133760851+jerry-xon@users.noreply.github.com>
2024-08-01 11:23:11 +0530

    Update stackusingarray.c

--- src/C/Sturcts/stackusingarray.c
+++ src/C/Sturcts/stackusingarray.c
@@ -33,7 +33,7 @@ void Display(){
     }else{
         printf("\nStack elements are:\n");
         for(int i=top;i>=0;i--){
-            printf("%d\n ",Stack[i]);
+            printf("%d\n",Stack[i]);
         }
     }
 }
