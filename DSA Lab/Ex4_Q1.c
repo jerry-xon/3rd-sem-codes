2bac2c8174a030d62cec784c78bd85d06027702b
Jayesh Purswani <133760851+jerry-xon@users.noreply.github.com>
2024-08-11 05:33:48 +0530

    List update

--- DSA Lab/Ex4_Q1.c
+++ DSA Lab/Ex4_Q1.c
@@ -7,55 +7,86 @@ typedef struct List
     int size;
 } List;
 
-//Initialize List
-void initList(List *list){
+// Initialize List
+void initList(List *list)
+{
     list->size = 0;
 }
 
-//Add element to the end of the list
-void add(List *list, int value){
-    if(list->size < MAX_SIZE){
+// Add element to the end of the list
+void add(List *list, int value)
+{
+    if (list->size < MAX_SIZE)
+    {
         list->data[list->size] = value;
         list->size++;
-}
-else{
-    printf("List is full. Cannot add more elements.\n");
-}
+    }
+    else
+    {
+        printf("List is full. Cannot add more elements.\n");
+    }
 }
 
 // Remove an element from the list at a specific index
-void removeAt(List *list, int index){
-    if(index == 0 || index <= list->size){
-        for(int i = index; i < list->size-1 ;i++){
-            list->data[i] = list->data[i+1];
+void removeAt(List *list, int index)
+{
+    if (index == 0 || index <= list->size)
+    {
+        for (int i = index; i < list->size - 1; i++)
+        {
+            list->data[i] = list->data[i + 1];
         }
         list->size--;
     }
-    else{
+    else
+    {
         printf("Index out of range.\n");
     }
 }
 
 // Get an element from the list at a specific index
-int get1(List *list, int index){
-    if(index >= 0 && index < list->size){
+int get1(List *list, int index)
+{
+    if (index >= 0 && index < list->size)
+    {
         return list->data[index];
     }
-    else{
+    else
+    {
         printf("Index out of range.\n");
         return -1;
     }
 }
 // Print the elements of the list
-void printList(List *list) {
-    if (list->size == 0) {
+void printList(List *list)
+{
+    if (list->size == 0)
+    {
         printf("List is empty.\n");
         return;
     }
-    for (int i = 0; i < list->size; i++) {
+    for (int i = 0; i < list->size; i++)
+    {
         printf("%d ", list->data[i]);
     }
     printf("\n");
+}
+
+void insertele(List *list, int index, int value)
+{
+    if (index >= 0 && index <= list->size && list->size < MAX_SIZE)
+    {
+        for (int i = list->size; i > index; i--)
+        {
+            list->data[i] = list->data[i - 1];
+        }
+        list->data[index] = value;
+        list->size++;
+    }
+    else
+    {
+        printf("Index out of range or list is full.\n");
+    }
 }
 
 int main()
@@ -64,13 +95,15 @@ int main()
     initList(&list);
     int choice, value, index;
 
-    while(1){
+    while (1)
+    {
         printf("\n-------------List Operations-------------\n");
         printf("1. Add element to the list\n");
         printf("2. Remove element from index of the list\n");
         printf("3. Get element from the list\n");
         printf("4. Print the list\n");
-        printf("5. Exit\n");
+        printf("5. Insert element at a specific index\n");
+        printf("6. Exit\n");
         printf("-------------Enter your choice-------------\n");
         scanf("%d", &choice);
 
@@ -91,8 +124,9 @@ int main()
         case 3:
             printf("Enter the index to get element: ");
             scanf("%d", &index);
-            value = get1(&list ,index);
-            if(value != -1){
+            value = get1(&list, index);
+            if (value != -1)
+            {
                 printf("Element at index %d is %d\n", index, value);
             }
             break;
@@ -101,8 +135,15 @@ int main()
             printList(&list);
             break;
         case 5:
-        printf("Exiting....");
-        return 0;
+            printf("Enter the index to insert at: ");
+            scanf("%d", &index);
+            printf("Enter the value to insert: ");
+            scanf("%d", &value);
+            insertAt(&list, index, value);
+            break;
+        case 6:
+            printf("Exiting....");
+            return 0;
 
         default:
             printf("Invalid choice. Please try again.\n");
