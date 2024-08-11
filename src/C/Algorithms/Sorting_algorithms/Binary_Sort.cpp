0df120c45e4206a6692e0ab814ef27b2460841c6
Nimitt Sharma <84977996+Nimittxo@users.noreply.github.com>
2024-08-11 06:02:06 +0530

    Create Binary_Sort.cpp

---
+++ src/C/Algorithms/Sorting_algorithms/Binary_Sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(const vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    
+    return -1;  // Target not found
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int target = 7;
+    int result = binarySearch(arr, target);
+    
+    if (result != -1) {
+        cout << "Element found at index " << result << endl;
+    }
+    else {
+        cout << "Element not found" << endl;
+    }
+    
+    return 0;
+}
